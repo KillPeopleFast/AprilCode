7ca968caecea2782eb188e6e5b81951208eb0768
game110pm <game110pm@605-110-STU05>
2017-04-21 13:46:40 -0600

    colorchange

--- Pacman/Pacman/Source.cpp
+++ Pacman/Pacman/Source.cpp
@@ -6,18 +6,27 @@
 #include <allegro5\allegro_acodec.h>
 #include <allegro5/allegro_image.h>
 using namespace std;
+int wallCollide(int x, int y, int dir, int level[20][20]);
 
-void wallCollide(int x, int y, int dir, int map[20][20]);
+const int RIGHT = 1;
+const int LEFT = 2;
+const int UP = 3;
+const int DOWN = 4;
+
+const int PACSIZE = 34;
+const int DISPLAY_HEIGHT = 800;
+const int DISPLAY_WIDTH = 800;
 
 int main()
-{
+{//sorsha is working on score next
 	ALLEGRO_DISPLAY *display = NULL;
 	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 	ALLEGRO_TIMER *timer = NULL;
 	ALLEGRO_BITMAP *square = NULL;
 	ALLEGRO_BITMAP *wall = NULL;
 	ALLEGRO_BITMAP *dot = NULL;
-	ALLEGRO_FONT *font = NULL;
+	//ALLEGRO_FONT *font = NULL;
+
 
 	//these two variables hold the x and y positions of the square
 	//initalize these variables to where you want your square to start
@@ -51,10 +60,10 @@ int main()
 		1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
 	};
 	for (int row = 0; row < 20; row++) {
-		for (int column = 0; column < 20; column++) 
+		for (int column = 0; column < 20; column++)
 			cout << map[row][column];
-			cout << endl;
-		
+		cout << endl;
+
 	}
 
 
@@ -71,15 +80,17 @@ int main()
 	bool doexit = false;
 
 	al_init();
-	//al_init_ttf_addon();
-	//al_init_font_addon();
+	al_init_ttf_addon();
+	al_init_font_addon();
 	//al_install_audio();
 	//al_init_acodec_addon();
 	al_init_image_addon();
 
 	//font = al_load_ttf_font("meatloaf.ttf", 100, 0);
 
-	//square = al_load_bitmap("pacman.jpg");
+	square = al_load_bitmap("pacman.jpg");
+	//if (square == NULL)
+	//        cout << "a;sldfkjas;ldfkjas;dlfkja" << endl;
 
 
 	//get the keyboard ready to use
@@ -87,21 +98,22 @@ int main()
 
 	timer = al_create_timer(.02);
 
-	display = al_create_display(800, 800);
+	display = al_create_display(DISPLAY_HEIGHT, DISPLAY_WIDTH);
 
-	square = al_create_bitmap(34, 34);
+	//square = al_create_bitmap(34, 34);
 
-	al_set_target_bitmap(square);
+	//al_set_target_bitmap(square);
 
-	al_clear_to_color(al_map_rgb(200, 200, 0));
+	//al_clear_to_color(al_map_rgb(200, 200, 0));
 
 	wall = al_create_bitmap(40, 40);
 
 	al_set_target_bitmap(wall);
 
-	al_clear_to_color(al_map_rgb(20, 200, 360));
+	al_clear_to_color(al_map_rgb(0, 0, 360));
 
 	dot = al_create_bitmap(16, 16);
+
 
 	al_set_target_bitmap(dot);
 
@@ -124,12 +136,14 @@ int main()
 	al_flip_display();
 
 	al_start_timer(timer);
-	
+
 
 	//so the game loop is set to act on "ticks" of the timer OR keyboard presses 
 	//OR the mouse closing the display
 	while (!doexit)
 	{
+		cout << square_x << " , " << square_y << endl;
+
 		ALLEGRO_EVENT ev;
 		al_wait_for_event(event_queue, &ev);
 
@@ -139,24 +153,31 @@ int main()
 		if (ev.type == ALLEGRO_EVENT_TIMER) {
 			//if the up button is pressed AND we're still below the top wall,
 			//move the box "up" by 4 pixels
-			if (key[0] && square_y >= 0) {
+			if (key[0] && wallCollide(square_x, square_y, UP, map) == 0) {
+
 				square_y -= 4.0;
 			}
 
 			//if the down button is pressed AND we're still above the bottom wall
 			//move the box "down" by 4 pixels
-			if (key[1] && square_y <= 800 - 32) {
+			if (key[1] && wallCollide(square_x, square_y, DOWN, map) == 0) {
 				square_y += 4.0;
 			}
 			//if the left button is pressed AND we're still right of the left wall
 			//move the box left by 4 pixels
-			if (key[2] && square_x >= 0) {
+			if (key[2] && wallCollide(square_x, square_y, LEFT, map) == 0) {
+
 				square_x -= 4.0;
 			}
+			if (!(square_x >0  && square_x <362  && square_y < 402 && square_y>0)){
+				square_x = 764;
+				square_y = 362;
+		}
 
-			//if the left button is pressed AND we're still left of the right wall
+			//if the right button is pressed 
 			//move the box right by 4 pixels
-			if (key[3] && square_x <= 800 - 32) {
+			if (key[3] && wallCollide(square_x, square_y, RIGHT, map) == 0) {
+
 				square_x += 4.0;
 			}
 
@@ -164,9 +185,9 @@ int main()
 			redraw = true;
 		}
 		if (map[(square_y + 20) / 40][(square_x + 20) / 40] == 2) {
-				map[(square_y + 20) / 40][(square_x + 20) / 40] = 4; //4s are blank
-				score++;
-			}
+			map[(square_y + 20) / 40][(square_x + 20) / 40] = 4; //4s are blank
+			score++;
+		}
 
 		///////////////////////////////////////////////////////////////////////////////////////////////////////
 		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
@@ -232,15 +253,6 @@ int main()
 		}
 
 
-		int PlatformCollide(int x, int y, int dir, int map[20][20]) {
-
-			if (((map[(x + 32) / 100][(y + 32) / 100] == 1) && dir == 1) ||
-
-				((map[(x) / 100][(y + 32) / 100] == 1) && dir == 1)) {
-
-				return 1; //collision when falling down!
-			}
-
 
 		//RENDER SECTION
 		//if the clock ticked but no other events happened, don't bother redrawing
@@ -252,7 +264,7 @@ int main()
 
 			//the algorithm above just changes the x and y coordinates
 			//here's where the bitmap is actually drawn somewhere else
-			al_draw_bitmap(square, square_x, square_y, 0);
+
 
 			//draw the MATRIX
 			for (int row = 0; row < 20; row++) {
@@ -260,11 +272,11 @@ int main()
 					if (map[row][column] == 1)
 						al_draw_bitmap(wall, column * 40, row * 40, NULL);
 					else if (map[row][column] == 2)
-						al_draw_bitmap(dot, column * 40+16, row * 40+16, NULL);
-					}
-			//al_draw_bitmap(square, 200, 200, 0);
+						al_draw_bitmap(dot, column * 40 + 16, row * 40 + 16, NULL);
+			}
+			al_draw_bitmap(square, square_x, square_y, 0);
 			//al_draw_textf(font, al_map_rgb(255, 255, 255), 69, 40, ALLEGRO_ALIGN_LEFT, "%i", score);
-			
+
 			al_flip_display();
 		}
 	}
@@ -277,7 +289,76 @@ int main()
 	return 0;
 }
 
-void wallCollide(int x, int y, int dir, int map[20][20])
+int wallCollide(int x, int y, int dir, int map[20][20])
 {
-	
+	int new_x1, new_x2, new_x3, new_y1, new_y2, new_y3;
+
+	if (dir == RIGHT) {         // Moving Right
+								// Check along the far right side of the sprite, plus 3 (the amount we’re moving)
+		new_x1 = x + 3 + PACSIZE;
+		new_x2 = x + 3 + PACSIZE;
+		new_x3 = x + 3 + PACSIZE;
+		// Check at three point along that edge
+		new_y1 = y;
+		new_y2 = y + PACSIZE / 2;
+		new_y3 = y + PACSIZE;
+
+		if (map[new_y1 / 40][new_x1 / 40] == 1 || map[new_y2 / 40][new_x2 / 40] == 1 || map[new_y3 / 40][new_x3 / 40] == 1) //add the other two points here
+			return 1; //collision!
+
+
+	}
+	if (dir == LEFT) {         // Moving left
+								// Check along the far right side of the sprite, plus 3 (the amount we’re moving)
+		new_x1 = x - 3;
+		new_x2 = x - 3;
+		new_x3 = x - 3;
+		// Check at three point along that edge
+		new_y1 = y;
+		new_y2 = y + PACSIZE / 2;
+		new_y3 = y + PACSIZE;
+
+
+
+		if (map[new_y1 / 40][new_x1 / 40] == 1 || map[new_y2 / 40][new_x2 / 40] == 1 || map[new_y3 / 40][new_x3 / 40] == 1) //add the other two points here
+			return 1; //collision!
+
+
+	}
+	if (dir == UP) {         // Moving up
+							   // Check along the far right side of the sprite, plus 3 (the amount we’re moving)
+		new_y1 = y - 3;
+		new_y2 = y - 3;
+		new_y3 = y - 3;
+		// Check at three point along that edge
+		new_x1 = x ;
+		new_x2 = x + PACSIZE / 2;
+		new_x3 = x + PACSIZE;
+
+
+
+		if (map[new_y1 / 40][new_x1 / 40] == 1 || map[new_y2 / 40][new_x2 / 40] == 1 || map[new_y3 / 40][new_x3 / 40] == 1) //add the other two points here
+			return 1; //collision!
+
+
+	}
+	if (dir == DOWN) {         // Moving down
+							   // Check along the far right side of the sprite, plus 3 (the amount we’re moving)
+		new_y1 = y + PACSIZE + 4;
+		new_y2 = y + PACSIZE + 4;
+		new_y3 = y + PACSIZE + 4;
+		// Check at three point along that edge
+		new_x1 = x;
+		new_x2 = x + PACSIZE / 2;
+		new_x3 = x + PACSIZE;
+
+
+
+		if (map[new_y1 / 40][new_x1 / 40] == 1 || map[new_y2 / 40][new_x2 / 40] == 1 || map[new_y3 / 40][new_x3 / 40] == 1) //add the other two points here
+			return 1; //collision!
+
+
+	}
+	return 0;
+
 }
---
+++ Screen changinn/Screen changinn/Source.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<ctime>
+using namespace std;
+void colorchange();//declaration
+int main() {
+	srand(static_cast<unsigned int>(time(0)));
+	while (1) {
+
+
+		colorchange();
+	}
+}
+
+void colorchange() {//definition
+	int num = rand() % 100 + 1;
+	if (num <= 25){
+		system("COLOR 10");
+		system("pause");
+		cout << num;
+}
+	else if (num >= 26 && num <= 50) {
+		system("COLOR 20");
+		system("pause");
+		cout << num;
+	}
+	else if (num >= 50 && num <= 100) {
+
+		system("COLOR 40");
+		cout << num;
+		
+		system("pause");
+	}
+}
+/*
+0 = Black
+1 = Blue
+2 = Green
+3 = Aqua
+4 = Red
+5 = Purple
+6 = Yellow
+7 = White
+8 = Gray
+9 = Light Blue
+A = Light Green
+B = Light Aqua
+C = Light Red
+D = Light Purple
+E = Light Yellow
+F = Bright White
+*/
+
